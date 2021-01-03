006f099bfd7abe5b28305b083d46eef0ab99f7ed
umarcor <unai.martinezcorral@ehu.eus>
2021-01-03 08:00:47 +0100

    update debian to 5.2+dfsg-3

--- run.sh
+++ run.sh
@@ -314,7 +314,7 @@ build_cfg () {
   FEDORA_VERSION="4.fc34"
   DEF_FEDORA_VERSION="5.2.0"
 
-  DEBIAN_VERSION="+dfsg-2"
+  DEBIAN_VERSION="+dfsg-3"
   DEF_DEBIAN_VERSION="5.2"
 
   case "$BUILD_ARCH" in
